ffdb284fb99add90a94e6d61aefc6cad6a7cad31
Zeeshan Ahmed <muhammadzeeshan26.mz@gmail.con>
2021-09-29 13:13:08 +0500

    converted html into small ejs templates

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title : 'Home' });
 })
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title : 'About' });
 })
 app.get('/about-me', (req, res) => {
-    res.redirect('/about');
+    res.status(301).redirect('/about');
 })
 app.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title : 'Create' });
 })
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title : '404' });
 })
 
 app.listen(3000, () => {
